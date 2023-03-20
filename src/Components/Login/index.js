2cea4ee4e7f80375e4b95d067a66a5c2b7a16fc9
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-20 18:55:29 +0000

    Login component created in react login folder

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Login = () =>{
-    return(<h1>Hello</h1>)
-
+    return(
+        <>
+            <form>
+                <input type="email" placeholder="email"/>
+                <input type="text" placeholder="password"/>
+                <input type="button" value="Login"/>
+                <input type="button" value="Sign Up"/>
+            </form>
+        </>
+    )
 }
 
 export default Login;
