5b30aa0bc2f88f9e7d7bf78608cb1c1f9f763b3b
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-22 00:23:18 +0000

    header added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Login from './Components/Login';
+import Header from './Components/Header';
 
 function App() {
   return (
+    <>
+    <Header/>
     <Login/>
-
+    </>
   );
 }
 
---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+
+
+const Header = () =>{
+
+    return(
+        <header className="header">
+            <h1>Social Media</h1>
+        </header>
+    )
+
+}
+
+export default Header;
