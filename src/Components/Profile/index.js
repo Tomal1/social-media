d7f6c748151f0d6c20222f4276de8c42aab07943
Minhazul Islam <minhazulislam@hotmail.com>
2023-03-31 13:19:29 +0100

    profile added with some content

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,12 +1,30 @@
 import React from "react";
+import "./profile.css"
 
 
 const profile = () => {
     return(
         <>
-            <div>
-                <h1>hello world</h1>
+            <div className="ProfileCon">
+
+                <form className="ProfilePicCon">
+                    <label>Profile Picture</label>
+                    <img className="ProfilePic" alt="picture of person who likes to rant"/>
+                </form>
+
+                <form className="AboutCon">
+                    <label>About</label>
+                    <textarea type="textarea" rows="8" cols="30" placeholder="Tell the world something about yourself"/>
+                    <input type="submit" value="Done" className="AboutBTN Post"/>
+                </form>
+
+                <form className="RantCon">
+                    <label>what is your Rant?</label>
+                    <textarea type="textarea" rows="8" cols="30" placeholder="What are you ranting on about now?"/>
+                    <input type="submit" value="Done" className="RantBTN Post"/>
+                </form>
             </div>
+            
         </>
     )
 }
