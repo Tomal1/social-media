b08b92e4dfa9a1ec4fd5795e9bc90a196c649430
Minhazul Islam <minhazulislam@hotmail.com>
2023-04-01 14:03:49 +0100

    login valadation and state variable created in Login/index.js

--- src/Components/Login/Lvaladation.js
+++ src/Components/Login/Lvaladation.js
@@ -1,11 +1,28 @@
 const Lvalidation = (values) => {
-    
-    const errors = {}
+    // we are going to store our errors in this object
+    const error = {};
 
-    if(values.nameOfInput === "email" && !values.nameOfInput) {
-        alert("please enter email")
-    } else if (values.nameOfInput === "password" && !values.nameOfInput) {
-        alert("please enter password")
-    } 
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+
+    if(values.email === ""){
+        error.email = "Email field should not be empty";
+    }else if(!email_pattern.test(values.email)){
+        error.email = "Email Did not match"
+    } else {
+        error.email = "";
+    }
+
+    if(values.password === ""){
+        error.password = "Password field should not be empty";
+    }else if(!password_pattern.test(values.password)){
+        error.password = "Password Did not match"
+    } else {
+        error.password = "";
+    }
+
+    return error;
 
 }
+
+export default Lvalidation;
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,32 +1,40 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./login.css";
-
+import Lvalidation from "./Lvaladation";
 import axios from "axios";
 
 const Login = () =>{
 
+    // store the initial values for login
     const [values, setValues] = useState({
         email: "",
         password: ""
     })
+
+
 // navigate has to be in the function
 const navigate = useNavigate();
     
+
 //this function will track what the client dose and then store into the state object above
     const clientInput = (e) => {
 
-        setValues(value => ({...value, 
+        setValues(values => ({...values, 
             [e.target.nameOfInput]: //detect changes typed in by client
             [e.target.valueInputted] //then store that into value attribute for the corresponding input field
         }))
     }
 
-    // stop our form form doing the default thing which is to do nothing
+    const [errors, setErrors] = useState({})
+
+
     const handleSubmit = (e) => {
+        // stop our form form doing the default thing which is to do nothing
         e.preventDefault();
-
-
+        
+        //whatever is returned from Lvalidation we will store it as an object in errors state variable as an object
+        setErrors(Lvalidation(values));
         /*
         resolved: if you get "net::ERR_CONNECTION_REFUSED" in heroku when fetching data, it is because 
         you cant hard code URL like this: "http://localhost:3001" 
@@ -49,7 +57,10 @@ const navigate = useNavigate();
             <form className="loginform" onSubmit={handleSubmit}> 
                 <h2>Log In</h2>
                 <input type="email" placeholder="Email" className="Email" valueInputted={values.email} onChange={clientInput} nameOfInput="email"/>
+                {errors.email && <span className="text-danger">{errors.email}</span>}
                 <input type="password" placeholder="Password" valueInputted={values.password} onChange={clientInput} nameOfInput="password"/>
+                <span>{errors.password && <span>{errors.password}</span>}</span>
+                
                 <input type="submit" className="Login modalLinks" value="Log In"/>
                 <Link to="/Signup" className="SignUp modalLinks">Sign Up</Link>
             </form>
