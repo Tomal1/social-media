45865ca5b4811b0b9f9fd5cb45a9abd1e8222c1a
Minhazul Islam <minhazulislam@hotmail.com>
2023-04-01 21:22:08 +0100

    drop down menu added in profile

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,6 +7,16 @@ const Header = () =>{
     return(
         <header className="header">
             <h1>Social Media</h1>
+        
+                <li className="menu">Menu
+                    <ul>
+                        <li>News Feed</li>
+                        <li>Delete Account</li>
+                        <li>Log Out</li>
+                    </ul>
+                </li>
+
+     
         </header>
     )
 
