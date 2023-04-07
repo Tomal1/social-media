d639d175563605cc759c7d7d9c9d8e62579b1288
Minhazul Islam <minhazulislam@hotmail.com>
2023-04-07 17:16:39 +0100

    reated secondry table for profile information in schema/schema.sql with forign key relating to the users table

--- server/schema/schema.sql
+++ server/schema/schema.sql
@@ -2,11 +2,18 @@ DROP DATABASE IF EXISTS social_media;
 CREATE DATABASE social_media;
 USE social_media;
 
-
 CREATE TABLE signUp (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100),
     email VARCHAR (100),
     password VARCHAR(100) 
+);
+
+CREATE TABLE profile (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    picture BLOB,
+    about VARCHAR(1000),
+    profile_id INT,
+    FOREIGN KEY (profile_id) REFERENCES signUp(id)
 );
 
--- server/schema/seeds.sql
+++ server/schema/seeds.sql
@@ -2,4 +2,16 @@ INSERT INTO signUp ( username, email, password )
 VALUES ("GreatGatsby", "TGGatyy191@xyz.co.uk", "123aaa" ),
        ("Huckleberry", "HFinny@xyz.co.uk", "132bbb"),
        ("Solitude", "100YS@xyz.co.uk", "321ccc"),
-       ("Things", "TFA@xyz.co.uk", "xxx123ABC")
+       ("Things", "TFA@xyz.co.uk", "xxx123ABC");
+
+
+INSERT INTO profile ( id, picture, about)
+VALUES(4, LOAD_FILE('../../public/tomal.jpeg'), 
+"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a 
+type specimen book. It has survived not only five centuries, but also the leap 
+into electronic typesetting, remaining essentially unchanged. 
+It was popularised in the 1960s with the release of Letraset sheets containing 
+Lorem Ipsum passages, and more recently with desktop publishing software like 
+Aldus PageMaker including versions of Lorem Ipsum.");
